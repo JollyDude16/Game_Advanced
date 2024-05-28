834fbc759a1f9b8f456bb4a2075022bd58080446
GwangGyu <mgg16a1@naver.com>
2024-05-29 08:06:25 +0900

    사용자 인증 미들웨어 검수

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,14 @@
 import jwt from 'jsonwebtoken';
-import {prisma} from '../utils/prisma/index.js';
+import { prisma } from '../utils/prisma/index.js';
 
 //사용자 인증 미들웨어
 export default async function (req, res, next) {
     try { //1. 쿠키를 가져온다
         const { authorization } = req.cookies;
+
+        if (!authorization) {
+            throw new Error('인증 토큰이 없습니다.');
+        }
         //2. 베어러 토큰 형식인지 확인 + 배열 구조분의 할당!
         const [tokenType, token] = authorization.split(' ');
         if (tokenType !== 'Bearer') throw new Error('토큰 타입이 일치하지 않습니다.');
@@ -12,27 +16,28 @@ export default async function (req, res, next) {
         const decodedToken = jwt.verify(token, 'customized_secret_key');
         const accountId = decodedToken.accountId;
         //4. JWT의 'userId'기반으로 사용자 조회
-        const account = await prisma.users.findFirst({
-            where: {accountId: +accountId},
+        const account = await prisma.account.findFirst({
+            where: { accountId: +accountId },
         })
         //4-1. user 탐색 실패시
-        if(!account){
-            res.cookieClear('authorization');
+        if (!account) {
+            res.clearCookie('authorization');
             throw new Error('토큰 사용자가 존재하지 않습니다.');
         }
         //5. 'req.user'에 조회된 사용자 정보를 할당
-            req.account = account;
+        req.account = account;
         //6. 다음 미들웨어 실행.
         next();
     }
-    catch (error){
+    catch (error) {
         res.clearCookie('authorization');
-    switch(error.name){
-        case 'TokenExpiredError'://토큰 만료시
-            return res.status(401).json({message:'토큰이 만료되었습니다.'});
-        case 'JsonWebTokenError'://토큰 검증 실패시
-            return res.status(401).json({message:'토큰인증에 실패하셨습니다.'});
-    default:
-        return res.status(401).json({message: error.message ?? '비 정상적인 요청입니다.'});
-    }}
+        switch (error.name) {
+            case 'TokenExpiredError'://토큰 만료시
+                return res.status(401).json({ message: '토큰이 만료되었습니다.' });
+            case 'JsonWebTokenError'://토큰 검증 실패시
+                return res.status(401).json({ message: '토큰인증에 실패하셨습니다.' });
+            default:
+                return res.status(401).json({ message: error.message ?? '비 정상적인 요청입니다.' });
+        }
+    }
 }
