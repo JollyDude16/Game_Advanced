68a7ada8378227cbaea52edeead5965a62dfd0ac
GwangGyu <mgg16a1@naver.com>
2024-05-30 14:44:50 +0900

    아이템사세 조회 추가

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import AccountsRouter from './src/routes/accounts.router.js';
-import GameRouter from './src/routes/accounts.router.js';
+import GameRouter from './src/routes/game.router.js';
 
 dotenv.config();
 
@@ -12,7 +12,7 @@ const PORT = 3018;
 app.use(express.json());
 app.use(express.urlencoded({enxtended:false}));
 app.use(cookieParser());
-app.use('/api', [AccountsRouter]);
+app.use('/api', [AccountsRouter, GameRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
